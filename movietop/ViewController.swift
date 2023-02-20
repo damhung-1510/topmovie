4be107ee7ca738f72a3030c923651e5501d25152
hungdq <hungdq@newwave.vn>
2023-02-20 15:06:20 +0700

    init project

---
+++ movietop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movietop
+//
+//  Created by Dam Hung on 20/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
